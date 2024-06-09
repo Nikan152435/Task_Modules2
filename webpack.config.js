d2cd59c57efbe78b085339dfe99117079a90df9b
NikaN <NikaN7602@gmail.com>
2024-06-09 20:15:53 +0200

    README.md

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Очистка папки dist перед новым билдом
+  },
+  mode: 'development', // Добавьте это, чтобы установить режим разработки
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 
+            'css-loader',
+            { 
+                loader: 'file-loader', // Эта строка использует file-loader
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'css/',
+                },
+              }, 
+        ],
+      },
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    devMiddleware: {
+        publicPath: '/dist/',
+      },
+  },
+};
